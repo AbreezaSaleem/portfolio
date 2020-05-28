7440ed9043c9807dbc759454b746f5166dcc3bfd
AbreezaSaleem <abreeza.saleem@hotmail.com>
2020-05-29 02:31:40 +0500

    Skills portion created 🧾

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React from 'react';
-import styled from 'styled-components';
 import AOS from 'aos';
 import 'aos/dist/aos.css'; 
 
+import FloatingButton from './components/FloatingButton';
 import Intro from './sections/Intro';
 import Skills from './sections/Skills';
 import Projects from './sections/Projects';
 
-AOS.init();
+AOS.init({ once: true });
 
 function App() {
   return (
+    <>
+    <FloatingButton />
     <div>
       <Intro />
       <Skills />
-      <Projects />
+      {/* <Projects /> */}
     </div>
+    </>
   );
 }
 
